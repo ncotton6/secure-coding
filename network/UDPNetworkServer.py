728fe71efc7c041cdbcd492c808e27f10f75a8b2
Praful Konduru <prafulprakash1993@gmail.com>
2016-10-07 19:06:06 -0400

    Create UDPNetworkServer.py

---
+++ network/UDPNetworkServer.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+HOST = '127.0.0.1'
+PORT = 5005
+
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+
+
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#now keep talking with the client
+while 1:
+    # receive data from client (data, addr)
+    data, addr = s.recvfrom(1024)
+    if not data:
+        break
+    print "client send : ", data
+    reply = raw_input('Enter message to reply :')
+
+    s.sendto(reply , addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+
+s.close()
