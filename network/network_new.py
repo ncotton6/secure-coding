f588baea2206bec08611ab2d85210127454a6cd4
Praful Konduru <prafulprakash1993@gmail.com>
2016-10-21 18:09:32 -0400

    Added array to store the json objects

--- network/network_new.py
+++ network/network_new.py
@@ -2,9 +2,7 @@ import time
 import zlib
 import json
 
-# This serves as a persistent storage, to retreive message on way back.
-# We can directly append the text we are sending to this structure
-parent().store('messages', [])
+myList = []
 
 def send(index):
     # Sending the whole message as a json blob with the attributes time, data and index
@@ -17,7 +15,7 @@ def send(index):
         "index": index
     }
 
-    parent().fetch('messages').append(message)
+    myList.append(message)
     message = json.dumps(message).encode('utf-8')
     message = zlib.compress(message)
     op('udpout_from_master').sendBytes(message)
@@ -30,9 +28,7 @@ def receive(messageBytes):
     message = zlib.decompress(messageBytes)
     message = message.decode('utf-8')
     message = json.loads(message)
-
-    messages = parent().fetch('messages')
-    for m in messages:
+    for m in myList:
         if m['index'] == message['index']:
             receivetime = time.time()
             roundtrip = receivetime - message['senttime']
